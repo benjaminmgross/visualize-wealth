7e42cf837296e3560abdfd29d6b45711b49759b2
Benjamin M. Gross <benjaminmgross@yahoo.com>
2014-06-25 04:52:40 -0400

    Changed the conf.py file to include capability of local and rtfd.org

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,11 +14,16 @@
 
 import sys
 import os
-import sphinx_rtd_theme
 
-html_theme = "sphinx_rtd_theme"
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the

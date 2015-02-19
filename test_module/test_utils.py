6a61076763e7e73dc1c58ed0c228eefb3855d10a
benjaminmgross <benjaminmgross@yahoo.com>
2015-02-18 22:23:51 -0500

    added test_create_store_master_index and test_union_store_indexes and PASSED

--- test_module/test_utils.py
+++ test_module/test_utils.py
@@ -7,24 +7,20 @@
 .. moduleauthor:: Benjamin M. Gross <benjaminMgross@gmail.com>
 
 """
-
+import os
 import pytest
+import numpy
 import pandas
 import tempfile
 import datetime
 from pandas.util import testing
-import visualize_wealth.utils as analyze
+import visualize_wealth.utils as utils
+
 
 @pytest.fixture
-def store_dir():
-    return tempfile.mkstemp(suffix = '.h5', 
-                            dir = '../test_data/',
-                            text = False
-    )
-
-@pytest.fixture
-def populaten_store(store_dir):
-    store = pandas.HDFStore(store_dir[1], mode = 'w')
+def populate_store():
+    name = './test_data/tmp.h5'
+    store = pandas.HDFStore(name, mode = 'w')
 
     #two weeks of data before today, delete one week, then update
     delta = datetime.timedelta(14)
@@ -44,65 +40,24 @@ def populaten_store(store_dir):
                                     name = 'Close')
     )
     store.close()
-    return index
+    return {'name': name, 'index': index}
 
-def test_create_store_master_index(store_dir, populate_store):
-    index = populate_store
+def test_create_store_master_index(populate_store):
+    index = populate_store['index']
     index = pandas.Series(index, index = index)
 
-    master_index = utils.create_store_master_index(store_dir[1])
-    testing.assert_series_equal(master_index, index)
-
-def test_union_store_indexes(store_dir, populate_store):
-    store = pandas.HDFStore(store_dir[1], mode = 'r+')
-    index = populate_store
-    union = utils.union_store_index(store)
-    pandas.assert_index_equal(index, union)
+    utils.create_store_master_index(populate_store['name'])
+    store = pandas.HDFStore(populate_store['name'], mode = 'r+')
+    testing.assert_series_equal(store.get('IND3X'), index)
     store.close()
+    os.remove(populate_store['name'])
 
+def test_union_store_indexes(populate_store):
+    store = pandas.HDFStore(populate_store['name'], mode = 'r+')
+    index = populate_store['index']
+    union = utils.union_store_indexes(store)
+    testing.assert_index_equal(index, union)
+    store.close()
+    os.remove(populate_store['name'])
 
-
-    
-    
-    #create two tickers, test the master_index creation
-    #test the cash creation
-
-    #test the master_index update
-    #test the cash update
-
-
-
-
-@pytest.fixture
-def test_file():
-    return pandas.ExcelFile('./test_data/test_analyze.xlsx')
-
-@pytest.fixture
-def man_calcs(test_file):
-    return test_file.parse('calcs', index_col = 0)
-
-@pytest.fixture
-def stat_calcs(test_file):
-    return test_file.parse('results', index_col = 0)
-
-@pytest.fixture
-def prices(test_file):
-    tmp = test_file.parse('calcs', index_col = 0)
-    return tmp[['S&P 500', 'VGTSX']]
-
-def test_active_return(prices, stat_calcs):
-    man_ar = stat_calcs.loc['active_return', 'VGTSX']
-
-    testing.assert_almost_equal(man_ar, analyze.active_return(
-                                series = prices['VGTSX'],
-                                benchmark = prices['S&P 500'],
-                                freq = 'daily')
-    )
-
-def test_active_returns(man_calcs, prices):
-    active_returns = analyze.active_returns(series = prices['VGTSX'], 
-                                            benchmark = prices['S&P 500'])
-
-    testing.assert_series_equal(man_calcs['Active Return'], active_returns)
-
-
+#def test_create_store_cash(store_dir

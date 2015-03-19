3b3725a5c290a45f0c6ee626f90ed440686168bc
benjaminmgross <benjaminmgross@yahoo.com>
2015-03-19 11:41:33 -0400

    added the 'Value at Close / Value at Open' columns to the price panel in price_panel_from_weight_file

--- visualize_wealth/construct_portfolio.py
+++ visualize_wealth/construct_portfolio.py
@@ -726,6 +726,7 @@ def panel_from_weight_file(weight_df, price_panel, start_value):
     for beg, fin in zip(int_beg, int_fin):
     
         close = price_panel.loc[:, beg:fin, 'Close']
+        opn = price_panel.loc[:, beg:fin, 'Open']
         adj = price_panel.loc[:, beg:fin, 'Adj Close']
 
         n = len(close)
@@ -749,11 +750,15 @@ def panel_from_weight_file(weight_df, price_panel, start_value):
 
         adj_q = c0_ac0.mul(ac_c).mul(n0)
         p_val = adj_q.xs(fin).mul(close.xs(fin)).sum()
+        vac = adj_q.mul(close)
+        vao = adj_q.mul(opn)
        
         panel = pandas.Panel.from_dict({'ac_c': ac_c, 
                                         'c0_ac0': c0_ac0,
                                         'n0': n0, 
-                                        'Adj_Q': adj_q}
+                                        'Adj_Q': adj_q,
+                                        'Value at Close': vac,
+                                        'Value at Open': vao}
         )
 
         #set items and minor appropriately for pfp constructors
@@ -981,38 +986,6 @@ def pfp_from_blotter(panel_from_blotter, start_value = 1000.):
     
     return price_df
 
-def test_funs():
-    """
-    >>> import pandas.util.testing as put
-    >>> xl_file = pandas.ExcelFile('../tests/test_splits.xlsx')
-    >>> blotter = xl_file.parse('blotter', index_col = 0)
-    >>> cols = ['Close', 'Adj Close', 'Dividends']
-    >>> price_df = xl_file.parse('calc_sheet', index_col = 0)
-    >>> price_df = price_df[cols]
-    >>> split_frame = calculate_splits(price_df)
-
-    >>> shares_owned = blotter_and_price_df_to_cum_shares(blotter, 
-    ...     split_frame)
-    >>> test_vals = xl_file.parse(
-    ...     'share_balance', index_col = 0)['cum_shares']
-    >>> put.assert_almost_equal(shares_owned['cum_shares'].dropna(), 
-    ...     test_vals)
-    True
-    >>> f = '../tests/panel from weight file test.xlsx'
-    >>> xl_file = pandas.ExcelFile(f)
-    >>> weight_df = xl_file.parse('rebal_weights', index_col = 0)
-    >>> tickers = ['EEM', 'EFA', 'IYR', 'IWV', 'IEF', 'IYR', 'SHY']
-    >>> d = {}
-    >>> for ticker in tickers:
-    ...     d[ticker] = xl_file.parse(ticker, index_col = 0)
-    >>> panel = panel_from_weight_file(weight_df, pandas.Panel(d), 
-    ...     1000.)
-    >>> portfolio = pfp_from_weight_file(panel)
-    >>> manual_calcs = xl_file.parse('index_result', index_col = 0)
-    >>> put.assert_series_equal(manual_calcs['Close'], 
-    ...     portfolio['Close'])
-    """
-    return None
 
 if __name__ == '__main__':
 
